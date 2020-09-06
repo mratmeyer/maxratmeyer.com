d760862688a4968550c246fed78da16c73044f72
Max Ratmeyer <17440617+MaxRatmeyer@users.noreply.github.com>
2020-09-06 00:15:56 -0400

    Added Turbolinks

--- .eleventy.js
+++ .eleventy.js
@@ -60,6 +60,18 @@ module.exports = function(eleventyConfig) {
     return [...tagSet];
     });
 
+    eleventyConfig.setBrowserSyncConfig({
+        // scripts in body conflict with Turbolinks
+        snippetOptions: {
+          rule: {
+            match: /<\/head>/i,
+            fn: function(snippet, match) {
+              return snippet + match;
+            }
+          }
+        }
+    });
+
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/assets/");

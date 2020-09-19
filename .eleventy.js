fe57d2f8370de1a4ea6400f80aee629756f909dd
Max Ratmeyer <17440617+MaxRatmeyer@users.noreply.github.com>
2020-09-19 13:49:34 -0400

    Removed Turbolinks

--- .eleventy.js
+++ .eleventy.js
@@ -60,18 +60,6 @@ module.exports = function(eleventyConfig) {
     return [...tagSet];
     });
 
-    eleventyConfig.setBrowserSyncConfig({
-        // scripts in body conflict with Turbolinks
-        snippetOptions: {
-          rule: {
-            match: /<\/head>/i,
-            fn: function(snippet, match) {
-              return snippet + match;
-            }
-          }
-        }
-    });
-
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/assets/");
